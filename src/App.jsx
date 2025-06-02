eb7d88fcf23d57cce6947549839ce161bd3a62a9
Jackson-299 <havocjack2942002@gmail.com>
2025-06-02 16:46:07 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,24 @@ import HomePage from './pages/HomePage';
 const App = () => {
   const queryClient = new QueryClient();
   return (
-    <>
-      <div className='fixed bottom-0 left-0 top-0'>
-        <Devbar />
-      </div>
-      <div className='ml-[700px]'>
-        <QueryClientProvider client={queryClient}>
-          {/* <HomePage /> */}
-          <Outlet />
-          <ReactQueryDevtools initialIsOpen={false}/>
+  //   <>
+  //     <div className='fixed bottom-0 left-0 top-0'>
+  //       <Devbar />
+  //     </div>
+  //     <div className='ml-[700px]'>
+  //       <QueryClientProvider client={queryClient}>
+  //         {/* <HomePage /> */}
+  //         <Outlet />
+  //         <ReactQueryDevtools initialIsOpen={false}/>
         
-        </QueryClientProvider>
+  //       </QueryClientProvider>
         
-      </div>
-    </>
-  );
+  //     </div>
+  //   </>
+ <div>
+   <Outlet />
+ </div> 
+);
 };
 
 export default App;
